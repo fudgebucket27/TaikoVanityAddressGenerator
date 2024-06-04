5e49fdd380f83694ad7a24fb6691481a278e9810
Fudgey <fudgebucket27@gmail.com>
2024-06-04 15:56:14 +1000

    update comments

--- TaikoVanityAddressGenerator/Program.cs
+++ TaikoVanityAddressGenerator/Program.cs
@@ -14,7 +14,7 @@ class Program
   
     static void Main(string[] args)
     {
-        string deployerAddress = "0xe7d8df8F6546965A59dab007e8709965Efe1255d"; //This is the deploy address of taiko wallets
+        string deployerAddress = "0xe7d8df8F6546965A59dab007e8709965Efe1255d"; //This is the deploy address of taiko l2 wallet on mainnet
         string ownerAddress = "0x36Cd6b3b9329c04df55d55D41C257a5fdD387ACd"; //Replace with your EOA owner address
         string desiredPattern = "0x69420"; //Your desired pattern at the start of the address.
 
